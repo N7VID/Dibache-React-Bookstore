c834adffcc95df5a85650bb98bfc3f0fe1d77ba2
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-09-08 07:06:04 +0330

    fix: Router Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/index.routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <NextUIProvider>
-      <App />
+      <RouterProvider router={router} />
     </NextUIProvider>
   </StrictMode>
 );

ca7588ef4adefac7b8107e24da93d7d2bfa3257b
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-09-09 05:46:44 +0330

    feat: AuthHeader Style

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -4,9 +4,9 @@ import AuthFooter from "./AuthFooter";
 
 export default function AuthLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen font-yekan">
       <AuthHeader />
-      <main className="flex-grow">{<Outlet />}</main>
+      <main className="flex-grow bg-ghost-white">{<Outlet />}</main>
       <AuthFooter />
     </div>
   );

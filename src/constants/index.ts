89e878380a3bdaac407130e87dd3d22e5db5e38b
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-09-10 22:47:51 +0330

    feat: Endpoints constants & base url

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const BASE_URL = "http://localhost:8000";
+export const ENDPOINTS = {
+  AUTH: {
+    SIGNUP: "/api/auth/signup",
+    LOGIN: "/api/auth/login",
+    LOGOUT: "/api/auth/logout",
+  },
+  PRODUCTS: "/api/products",
+  CATEGORIES: "/api/categories",
+  SUBCATEGORIES: "/api/subcategories",
+  ORDERS: "/api/orders",
+  USERS: "/api/users",
+};

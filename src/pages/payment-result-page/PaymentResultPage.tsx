92aeae2258f2e1974d78483947f78ae71c45ea5d
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-10-10 21:40:14 +0330

    feat: Post order

--- src/pages/payment-result-page/PaymentResultPage.tsx
+++ src/pages/payment-result-page/PaymentResultPage.tsx
@@ -1,13 +1,43 @@
 import { Link } from "@nextui-org/react";
+import { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { PATHS } from "../../configs/paths.config";
+import { RootContext } from "../../context/RootContextProvider";
+import { usePostService } from "../../hooks/usePostService";
+import { postOrders } from "../../queryhooks/orders";
+import { toast } from "react-toastify";
 
 export default function PaymentResultPage() {
-  const searchParams = new URLSearchParams(useLocation().search).get("status");
+  const paymentStatus = new URLSearchParams(useLocation().search).get("status");
+  const context = useContext(RootContext);
+  if (!context)
+    throw new Error("useCart must be used within a RootContextProvider");
+  const { cart, setCart } = context;
+
+  const { mutate } = usePostService({
+    mutationKey: ["PostOrders"],
+    mutationFn: postOrders,
+    options: {
+      onSuccess: () => {
+        toast.success("سفارش شما ثبت گردید.");
+        setCart({ products: [], user: "" });
+        localStorage.removeItem("cart");
+      },
+      onError: (error) => {
+        toast.error(error.message, { rtl: false });
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (paymentStatus === "success" && cart.products.length > 0) {
+      mutate(cart);
+    }
+  }, [paymentStatus, cart, mutate]);
 
   return (
     <div className="LayoutContainer py-6 flex justify-center items-center cursor-default">
-      {searchParams === "success" ? (
+      {paymentStatus === "success" ? (
         <div className="flex flex-col items-center">
           <img
             src="/src/assets/images/payment-success.jpg"

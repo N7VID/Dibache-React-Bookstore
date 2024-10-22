31bcf52b82234b3c356fe92d803552fe0dee49c1
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-10-22 19:39:09 +0330

    fix: Layout subcategoryPage

--- src/pages/subcategory-page/SubcategoryPage.tsx
+++ src/pages/subcategory-page/SubcategoryPage.tsx
@@ -77,7 +77,7 @@ export default function SubcategoryPage() {
         handleSortOrder={handleSortOrder}
         currentParams={currentParams}
       />
-      <section className="py-4 flex flex-col gap-6">
+      <section className="flex flex-col gap-2 md:items-center md:justify-center">
         <div className="flex gap-4 px-6 py-8 overflow-x-auto scrollbar-hide">
           {isLoading ? (
             <Spinner size="lg" color="current" />

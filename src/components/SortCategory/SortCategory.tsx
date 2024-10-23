ed1568f7763da7b23d0ae3f20d25d114be00a260
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-10-23 12:22:45 +0330

    fix: Delete unnecessary font

--- src/components/SortCategory/SortCategory.tsx
+++ src/components/SortCategory/SortCategory.tsx
@@ -26,7 +26,7 @@ export default function SortCategory({
 }: Params) {
   return (
     <section className="w-full py-8 shadow-box bg-white rounded-lg my-4 px-10 flex justify-between items-center flex-col gap-8 md:flex-row">
-      <h3 className="font-bold text-xl font-vazir">{name}</h3>
+      <h3 className="font-bold text-xl font-yekan">{name}</h3>
       <div className="flex items-center flex-col gap-4 md:flex-row">
         <Input
           isClearable
